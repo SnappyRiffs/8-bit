e391d599f3a58662d76a14f987de07f1dfa54ec5
SnappyRiffs <97476091+SnappyRiffs@users.noreply.github.com>
2025-08-16 15:57:57 -0700

    Update implements.h

--- implements.h
+++ implements.h
@@ -1,8 +1,9 @@
-#ifndef implements
-#define implements
+#ifndef implements_H
+#define implements_H
 class implements{
     public:
         void def(mode,var);
         void def(mode,int var[num]);
 }
 #endif
+
