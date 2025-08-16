024d0d843e9e21f3dafef091a84f5ecd04d50e16
SnappyRiffs <97476091+SnappyRiffs@users.noreply.github.com>
2025-08-16 15:55:54 -0700

    Update _74LS245.cpp

--- _74LS245.cpp
+++ _74LS245.cpp
@@ -1,26 +1,25 @@
-#include "implements.h"
-#include <array>
+#include "_74LS245.h"
 
-class _74LS245 {
-    std::array<bool,8> transfer(bool DIR, bool OE, std::array<bool,8> A, std::array<bool,8> B) {
-        if (OE == false) { // active low OE
-            if (DIR == false) {
-                setMode(A, Input);
-                setMode(B, Output);
-                for (size_t i = 0; i < A.size(); i++) {
-                    B[i] = A[i];
-                }
-                return B;
-            } else {
-                setMode(B, Input);
-                setMode(A, Output);
-                for (size_t i = 0; i < B.size(); i++) {
-                    A[i] = B[i];
-                }
-                return A;
+std::array<bool,8> _74LS245::transfer(bool DIR, bool OE, 
+                                      std::array<bool,8> A, 
+                                      std::array<bool,8> B) {
+    if (OE == false) { // active low OE
+        if (DIR == false) {
+            setMode(A, Input);
+            setMode(B, Output);
+            for (size_t i = 0; i < A.size(); i++) {
+                B[i] = A[i];
             }
+            return B;
+        } else {
+            setMode(B, Input);
+            setMode(A, Output);
+            for (size_t i = 0; i < B.size(); i++) {
+                A[i] = B[i];
+            }
+            return A;
         }
-        // If OE is high (disabled), return unchanged bus
-        return {};
     }
-};
+    // If OE is high (disabled), return empty bus
+    return {};
+}
