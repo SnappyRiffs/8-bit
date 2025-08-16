e8f2922d34cc89387ec95be2a2637558f40374aa
SnappyRiffs <97476091+SnappyRiffs@users.noreply.github.com>
2025-08-16 15:54:39 -0700

    Update and rename 74LS245.8bit to _74LS245.cpp

---
+++ _74LS245.cpp
@@ -0,0 +1,26 @@
+#include "implements.h"
+#include <array>
+
+class _74LS245 {
+    std::array<bool,8> transfer(bool DIR, bool OE, std::array<bool,8> A, std::array<bool,8> B) {
+        if (OE == false) { // active low OE
+            if (DIR == false) {
+                setMode(A, Input);
+                setMode(B, Output);
+                for (size_t i = 0; i < A.size(); i++) {
+                    B[i] = A[i];
+                }
+                return B;
+            } else {
+                setMode(B, Input);
+                setMode(A, Output);
+                for (size_t i = 0; i < B.size(); i++) {
+                    A[i] = B[i];
+                }
+                return A;
+            }
+        }
+        // If OE is high (disabled), return unchanged bus
+        return {};
+    }
+};
