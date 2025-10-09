12748415e2facf7f5b900a08d5d91256e354c35e
SnappyRiffs <jadonjung3@gmail.com>
2025-10-09 08:29:54 -0700

    Update eater.cpp

--- eater.cpp
+++ eater.cpp
@@ -1,28 +1,32 @@
-#include "_74LS245.h"
+#include "OctalBusTransceivers.h"
 #include "implements.h"
+#include <array>
 
-class EightBit{
-	int clock(int HLT, int mode, int CLK, int SS){
-		int out = 0;
-		if (!HLT){	
-			if (mode){
-				out = CLK;
-			}
-			else{
-				out = SS;
-			}
-		}
-		return out;
-	}
-	std::array<int, 8> reg_A (int AI, int AO, int CLK, int CLR, std::array<int, 8> BUS){
-		return BUS; // placeholder
+class EightBit {
+private:
+    std::array<int, 8> BUS_;
 
-		auto result = _74LS245::transfer(AI, AO, BUS, BUS);
-	}
+public:
+    int clock(int HLT, int mode, int CLK, int SS) {
+        int out = 0;
+        if (!HLT) {	
+            if (mode) {
+                out = CLK;
+            } else {
+                out = SS;
+            }
+        }
+        return out;
+    }
 
-	std::array<int, 8> reg_B (int BI, int BO, int CLK, int CLR, std::array<int, 8> BUS){
-		return reg_A(BI, BO, CLK, CLR, BUS);
-	}
-}
+    std::array<int, 8> reg_A(int AI, int AO, int CLK, int CLR, std::array<int, 8> BUS) {
+        // Assign BUS to BUS_
+        BUS_ = BUS;
+        return BUS_;
+    }
 
-
+    std::array<int, 8> reg_B(int BI, int BO, int CLK, int CLR, std::array<int, 8> BUS) {
+        // Just reuses reg_A
+        return reg_A(BI, BO, CLK, CLR, BUS);
+    }
+};
