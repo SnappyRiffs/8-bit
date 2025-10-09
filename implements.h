fcb6f5fd70fd74ca01791384e59d187c4d8ea18c
SnappyRiffs <jadonjung3@gmail.com>
2025-10-09 08:32:24 -0700

    Update implements.h

--- implements.h
+++ implements.h
@@ -2,11 +2,14 @@
 #define implements_H
 
 #include <Arduino.h>
+#include <array>
 
 class implements
 {
 public:
     void setMode(int pin, int mode);
     void setMode(int pins[8], int mode);
+    typedef std::array<int, 8> Array;
 }
 #endif
+
