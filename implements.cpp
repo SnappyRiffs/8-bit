41b949e2159c5df9a458f81a860550bcc862c743
SnappyRiffs <jadonjung3@gmail.com>
2025-10-09 08:33:45 -0700

    Update implements.cpp

--- implements.cpp
+++ implements.cpp
@@ -1,11 +1,13 @@
-#include <implements.h>
+#include "implements.h"   // use quotes for your own header
 #include <Arduino.h>
 
+// Set mode for a single pin
 void implements::setMode(int pin, int mode)
 {
     pinMode(pin, mode);
 }
 
+// Set mode for an array of 8 pins
 void implements::setMode(int pins[8], int mode)
 {
     for (int i = 0; i < 8; ++i)
